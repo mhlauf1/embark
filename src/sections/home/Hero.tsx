9e912100e721d24337765a5cf196cb385ed1ae27
mhlauf1 <mlaufersweiler@gmail.com>
2025-02-06 22:45:19 -0600

    adding overflow x hidden

--- src/sections/home/Hero.tsx
+++ src/sections/home/Hero.tsx
@@ -69,7 +69,7 @@ const Hero = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ duration: 0.8, delay: 0.2 }}
-              className="relative rounded-xl overflow-hidden"
+              className="relative rounded-md md:rounded-xl overflow-hidden"
             >
               <video
                 autoPlay
