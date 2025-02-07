4499fa32f12fe79fd2649b81d28fb97637dac2c3
mhlauf1 <mlaufersweiler@gmail.com>
2025-02-07 10:28:20 -0600

    updating the partner with us page

--- src/sections/partner/Hero.tsx
+++ src/sections/partner/Hero.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="bg-[#D5F0FF] relative px-4 sm:px-8 md:px-24 h-auto flex items-center pt-6 md:pt-12 pb-24 md:pb-36 flex-col w-full">
-      <div className="items-center justify-center flex  w-full flex-col py-12 ">
-        <div className="flex flex-col justify-center  items-center">
-          <div className="border border-[#183453]/50 bg-white rounded-full py-1 flex flex-row items-center gap-2 px-3">
+    <section className="bg-[#D5F0FF] relative min-h-screen">
+      <div className=" px-4 sm:px-8 md:px-24 py-12 md:py-24">
+        <div className="flex flex-col items-center">
+          <div className="border border-[#183453]/50 bg-white rounded-full py-1 flex items-center gap-2 px-3">
             <Image
               src="/badge-paws.png"
               height={18}
@@ -16,28 +16,30 @@ const Hero = () => {
             />
             <p className="font-semibold capitalize">Join the network</p>
           </div>
-          <h1 className="text-4xl sm:text-5xl px-4 md:text-6xl lg:text-7xl font-bold mt-5 md:mt-8 tracking-tight text-center capitalize">
+
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mt-5 md:mt-8 tracking-tight text-center capitalize">
             We want to partner with you
           </h1>
-          <p className="pt-5 px-4 md:pt-8 text-lg text-center md:text-xl max-w-[72ch] font-semibold">
+
+          <p className="pt-5 md:pt-8 text-lg md:text-xl max-w-[72ch] text-center font-semibold">
             Embark is actively looking to team up with the best pet daycare,
             boarding, and grooming providers in the US. We can develop flexible
             structures to accommodate sellers&apos; goals as they take the next
             step with their business
           </p>
-          <div className="flex items-center mt-12 px-4 flex-row gap-2">
+
+          <div className="flex items-center w-full md:w-auto mt-12 flex-col sm:flex-row gap-4">
             <input
               placeholder="Enter your email"
               type="email"
-              className="w-[400px] py-3 px-3 rounded-md"
+              className="w-full sm:w-[400px] py-4 border border-[#183453]/20 px-3 rounded-md"
             />
-            <button className="font-medium  border border-[#183453]/20 tracking-wide rounded-md py-4 px-8 bg-[#9CD8F6]">
+            <button className="w-full sm:w-auto font-medium border border-[#183453]/20 tracking-wide rounded-md py-4 px-8 bg-[#9CD8F6]">
               Submit
             </button>
           </div>
-        </div>
-        <div className="relative mt-12 w-full">
-          <div className="relative">
+
+          <div className="relative mt-12 w-full">
             <div className="w-full relative h-[700px]">
               <Image
                 src="/partner-hero.png"
@@ -47,14 +49,14 @@ const Hero = () => {
                 sizes="100vw"
               />
             </div>
+            <Image
+              className="absolute top-[-30px] md:top-[-60px] -rotate-12 md:rotate-0 left-0 md:left-[-60px]"
+              src="/white-badge.svg"
+              alt="Embark sticker"
+              height={170}
+              width={170}
+            />
           </div>
-          <Image
-            className="absolute bottom-[-60px] left-[-60px]"
-            src="/hero-badge.png"
-            alt="Embark sticker"
-            height={170}
-            width={170}
-          />
         </div>
       </div>
     </section>
