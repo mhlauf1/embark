ad6e79e1f1ebfe0bc7a1a0e580c9c25d08fbad38
mhlauf1 <mlaufersweiler@gmail.com>
2025-02-10 16:27:40 -0600

    sizing

--- src/sections/services/ServicesList.tsx
+++ src/sections/services/ServicesList.tsx
@@ -6,7 +6,7 @@ import Health from "./Heath";
 const ServicesList = () => {
   return (
     <section className="py-24 md:py-36 border bg-gradient-to-b from-[#F9FDFF] relative to-[#EEF9FF]">
-      <div className="gap-24 md:gap-36 px-4 sm:px-8 md:px-24 flex flex-col">
+      <div className="gap-16 md:gap-36 px-4 sm:px-8 md:px-24 flex flex-col">
         <Daycare />
         <Boarding />
         <Grooming />
