fff4f125735a586c0510da62069fcb67443757e9
mhlauf1 <mlaufersweiler@gmail.com>
2025-04-01 23:17:03 -0500

    adding:

--- src/sections/about/OurVision.tsx
+++ src/sections/about/OurVision.tsx
@@ -70,7 +70,7 @@ const Vision = () => {
                   src="/paw-small.svg"
                   height={16}
                   width={16}
-                  alt="Embark Pet Services Badge Paw Prints"
+                  alt="Embark Pet Services Badge Paw Prints About"
                 />
                 <p className="font-semibold text-lg md:text-xl capitalize">
                   The Embark Way
