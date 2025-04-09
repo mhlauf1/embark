e1dfdfbc8f2fb42ca650f861f56d73f9fbb1f16b
mhlauf1 <mlaufersweiler@gmail.com>
2025-04-09 10:20:05 -0500

    changing config

--- next.config.ts
+++ next.config.ts
@@ -4,21 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   async redirects() {
     return [
-      // Redirects need to start with / in Vercel
-      // For http to https, you'll need to handle this at the DNS/Vercel project level
-
-      // Redirect non-www to www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'embarkpetservices.com',
-          },
-        ],
-        destination: 'https://www.embarkpetservices.com/:path*',
-        permanent: true,
-      },
       // Redirect old WordPress paths
       {
         source: '/wp-content/:path*',
